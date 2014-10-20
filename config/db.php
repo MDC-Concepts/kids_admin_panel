4830118e002ad9e549cdc841fa3bf05b959eacf0
code-lord <vikrambghadge@gmail.com>
2014-10-20 06:36:51 -0700

    Kidsi First Commite

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=kidsi',
+    'username' => 'kidsi_user',
+    'password' => 'kidsi_user',
+    'charset' => 'utf8',
+];
